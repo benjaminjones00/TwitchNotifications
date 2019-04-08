230df40270749954f6aa407ce0579b400ab452d5
benjaminjones00 <benjaminjones00@protonmail.com>
2019-04-08 12:43:48 +0100

    v.1.0
    First working version of TwitchNotifications

--- TwitchNotifications.py
+++ TwitchNotifications.py
@@ -1,3 +1,4 @@
+import time
 import requests
 import json
 import csv
@@ -9,27 +10,50 @@ def get_client_id():
         client_id = list(reader)
         user_client_id = client_id[3][0][13:-1]
         return(user_client_id)
-client_id = get_client_id()
-  
+
 def live_check(streamer_name, client_id):
     twitch_api_stream_url = "https://api.twitch.tv/kraken/streams/" \
                             + streamer_name + "?client_id=" + client_id
     streamer_html = requests.get(twitch_api_stream_url)
     streamer = json.loads(streamer_html.content)
     if streamer['stream'] == None:
-        return('Offline')
+        return(False)
     else:
-        return('Online')
-        
-def toast_notification():
+        return(True)
+
+def toast_notification(streamer_name):
     toaster = ToastNotifier()
-    toaster.show_toast("Twitch Notification", streamer_name + " streamer has gone live", icon_path="twitch.ico", threaded=True)
+    toaster.show_toast("Twitch Notification", streamer_name + " is now live!", icon_path="twitch.ico", threaded=True)
     while toaster.notification_active(): time.sleep(0.1)
-        
-with open('streamer_list.csv', 'r') as f:
-  reader = csv.reader(f)
-  your_list = list(reader)
-  test = your_list[0] [0:]
-  print(test)
-  for test in test:
-      live_check(test, client_id)
+
+client_id = get_client_id()
+
+notify = []
+online = []
+
+with open('streamer_list.csv', 'r') as streamer_list_file:
+    reader = csv.reader(streamer_list_file)
+    reader_list = list(reader)
+    streamer_list = reader_list[0] [0:]
+offline = streamer_list[0:]
+
+
+while True:
+    for x in offline:
+        if live_check(x, client_id) is True:
+            notify.append(x)
+            offline.remove(x)
+        else:
+            pass
+
+    for x in notify:
+        toast_notification(x)
+        online.append(x)
+        notify.remove(x)
+
+    for x in online:
+        if live_check(x, client_id) is True:
+            pass
+        elif live_check(x, client_id) is False:
+            offline.append(x)
+            online.remove(x)
